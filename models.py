56357a279febc1658405daf313dbe2807b38dbd5
Balaji Sankarapu <192012023.sse@saveetha.com>
2025-07-12 01:27:20 +0530

    modfied

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
-# models.py
-from extensions import db
+from app import db
+from datetime import datetime
 
 class Verification(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    document_type = db.Column(db.String(100))
+    filename = db.Column(db.String(100))
+    document_type = db.Column(db.String(20))
     extracted_text = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
